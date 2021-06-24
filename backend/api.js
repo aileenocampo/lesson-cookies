c51b83aabca1c7c62827bb0d97e3a14abba76b62
Aileen Ocampo <aileenocampo24@gmail.com>
2021-06-25 01:43:35 +1000

    add exisitng

--- backend/api.js
+++ backend/api.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser')
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
 
 //cors must be set to the react app's domain, and credentials allowed in order to successfully set cookies on the browser.
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
@@ -14,14 +16,22 @@ app.get('/', (_, res) => res.send('Hello World!'));
 
 //todo: Return an array containing the cookies from the request. - hint: Object.entries may come in handy.
 app.get('/api/cookies', (request, response) => {
+    var cookies = Object.entries(request.cookies);
+    response.send(cookies);
 });
 
 //todo: Create a cookie with a random value.
 app.post('/api/cookies/random', (request, response) => {
+    const random = "A random cookie!" + Math.random();
+    response.cookie("random", random);
+    response.end();
 });
 
 //todo: Update the username cookie.
 app.put('/api/cookies/username', (request, response) => {
+    const username = "A user from the cookies!";
+    response.cookie("username", username);
+    response.end()
 });
 
 //example: This sets a cookie, used in the HeaderUserInfo.jsx file.
@@ -33,6 +43,8 @@ app.get('/api/cookies/username', (_, response) => {
 
 //todo: Delete the username cookie.
 app.delete('/api/cookies/username', (_, response) => {
+    response.clearCookie('username');
+    response.end();
 });
 
 const port = 5000;
